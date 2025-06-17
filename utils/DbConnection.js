23bd94ce6888a1a761d48b908382c2961d3b409a
rudratank07 <rudratank07@gmail.com>
2025-06-17 22:51:08 +0530

    Db Bug Fix

--- utils/DbConnection.js
+++ utils/DbConnection.js
@@ -1,9 +1,13 @@
-const connection = (databaseurl) => {
-  return mongoose
-    .connect(databaseurl)
-    .then(() => console.log("Database Connection Successful..."))
-    .catch((err) => {
-      console.log("Database Connection Error", err);
-      throw err; // Rethrow so caller knows connection failed
-    });
+import mongoose from "mongoose";
+
+const connection = async (databaseurl) => {
+  try {
+    await mongoose.connect(databaseurl);
+    console.log("Database Connection Successful...");
+  } catch (err) {
+    console.log("Database Connection Error", err);
+    throw err;
+  }
 };
+
+export default connection;
